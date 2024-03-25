a6b540fc7a6b17ef98037ba2ad69aa8bf782712c
alejogymsystem <alejogymsystem@gmail.com>
2024-03-25 08:29:31 -0500

    2

--- models/usuario.js
+++ models/usuario.js
@@ -1,6 +1,7 @@
 const {Schema, model } = require('mongoose')
+const bcrypt = require('bcrypt');
 
-const UsuarioSchema = ({
+const UsuarioSchema = new Schema({
     Correo:{
         type: String,
         unique:true,
@@ -18,5 +19,35 @@ const UsuarioSchema = ({
         required:[true, 'El Iva producto es requirido']
     },
 });
+
+UsuarioSchema.pre('save', async function(next) {
+    const usuario = this;
+    if (!usuario.isModified('Contrasena')) return next();
+    
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(usuario.Contrasena, salt);
+        usuario.Contrasena = hashedPassword;
+        next();
+    } catch (error) {
+        return next(error);
+    }
+});
+
+UsuarioSchema.pre('save', async function(next) {
+    const usuario = this;
+    if (!usuario.isModified('Contrasena')) return next();
+    
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(usuario.Contrasena, salt);
+        usuario.Contrasena = hashedPassword;
+        next();
+    } catch (error) {
+        return next(error);
+    }
+});
+
+
 
 module.exports = model('Usuario', UsuarioSchema);
